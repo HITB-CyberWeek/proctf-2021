bd5b192d019bdc280da023fad1cb8c8fea575634
Dmitriy Titarenko <mail.dscheg@gmail.com>
2021-11-15 12:09:39 +0500

    [timecapsule] UI fixes

--- services/timecapsule/src/wwwroot/scripts.js
+++ services/timecapsule/src/wwwroot/scripts.js
@@ -1,32 +1,28 @@
 import { wrap, unwrap } from './wrapper.js';
 
-let $ = (selector, element) => { return (element || document).querySelector(selector); }
-
-let auth = false;
+let $ = (selector, element) => (element || document).querySelector(selector);
 let error = (msg) => alert(msg);
 
 fetch("/auth", {credentials:"same-origin"}).then(response => {
-	if(!response.ok) response.text().then(text => { error(text); });
+	if(!response.ok) response.text().then(text => error(text));
 	else {
 		response.text().then(text => {
 			$("#login").value = text;
 			$("#my").href = "/?author=" + encodeURIComponent(text);
-			auth = true;
+			$(".auth").classList.add("signed");
 		}).catch(() => error("/auth failed"));
 	}
 });
 [$("#signup"), $("#signin")].forEach(el => el.onclick = () => {
-	const url = `/signup?login=${encodeURIComponent($("#login").value)}&password=${encodeURIComponent($("#login").value)}`;
+	const url = `/signup?login=${encodeURIComponent($("#login").value)}&password=${encodeURIComponent($("#password").value)}`;
 	fetch(url, {method: "POST", credentials: "same-origin"}).then(response => {
 		if(!response.ok) response.text().then(text => error(text));
-		else {
-			console.log("signed");
-		}
+		else $(".auth").classList.add("signed");
 	});
 });
 
 function renderCapsule($body, template, item) {
-	$(".item", template).id = "id_" + item.id;
+	$(".item", template).id = "id_" + item.id.replace(/[^0-9a-f-]/g, "");
 	$(".author", template).textContent = item.author;
 	$(".created", template).textContent = (typeof item.createDate == "string" ? item.createDate : item.createDate.toISOString()).split(".")[0].replace("T", " ");
 	$(".expired", template).textContent = (typeof item.expireDate == "string" ? item.expireDate : item.expireDate.toISOString()).split(".")[0].replace("T", " ");
@@ -34,7 +30,7 @@ function renderCapsule($body, template, item) {
 	if(!item.secret || !item.secret.length)
 		$(".secret", template).classList.add("hidden");
 	$(".capsule", template).textContent = item.timeCapsule;
-	$("a", template).href = "capsule.html?id=" + encodeURIComponent(item.id);
+	$("a", template).href = "/capsule.html?id=" + encodeURIComponent(item.id);
 	if(!!item.text)
 		$(".text", template).textContent = item.text;
 	let clone = document.importNode(template, true);
@@ -49,50 +45,56 @@ function renderCapsule($body, template, item) {
 if(location.pathname == "/decrypt.html") {
 	$("form").onsubmit = e => {
 		e.preventDefault();
+
 		let key = $("#key").value;
 		let timeCapsule = $("#timecapsule").value;
+
 		const $body = $(".single");
 		while($body.lastElementChild){$body.removeChild($body.lastElementChild);}
-		unwrap(timeCapsule, key).then(capsule => {
-			renderCapsule($(".single"), $("template").content, capsule);
-		});
+		unwrap(timeCapsule, key).then(capsule => renderCapsule($(".single"), $("template").content, capsule));
 	}
 }
 
 if(location.pathname == "/add.html") {
+	if(!$("#tobeopened").value?.length) $("#tobeopened").value = new Date(new Date().getTime() + 600000).toISOString().replace("T", " ").split(".")[0];
 	$("form").onsubmit = e => {
 		e.preventDefault();
+
 		let toBeOpened = $("#tobeopened").value;
 		let text = $("#text").value;
 
 		fetch(`/capsule?text=${encodeURIComponent(text)}&tobeopened=${encodeURIComponent(toBeOpened)}`, {method:"POST", credentials:"same-origin"}).then(response => {
-			if(!response.ok) response.text().then(text => { error(text); });
+			if(!response.ok) response.text().then(text => error(text));
 			else {
 				response.json().then(json => {
 					if(!json) return;
+
 					const $body = $(".single");
 					while($body.lastElementChild){$body.removeChild($body.lastElementChild);}
+
 					renderCapsule($body, $("template").content, json);
-				}).catch((e) => error("/capsule failed" + e));
+				}).catch(e => error("post /capsule failed: " + e));
 			}
 		});
 	}
 }
 
 if(location.pathname == "/capsule.html") fetch(`/capsule/${encodeURIComponent(new URLSearchParams(location.search).get("id") || "")}`, {credentials:"same-origin"}).then(response => {
-	if(!response.ok) response.text().then(text => { error(text); });
+	if(!response.ok) response.text().then(text => error(text));
 	else {
 		response.json().then(json => {
 			if(!json) return;
+
 			const $body = $(".single");
 			while($body.lastElementChild){$body.removeChild($body.lastElementChild);}
+
 			renderCapsule($body, $("template").content, json);
-		}).catch((e) => error("/capsule failed" + e));
+		}).catch(e => error("get /capsule failed: " + e));
 	}
 });
 
 if(location.pathname == "/" || location.pathname == "index.html") fetch("/capsules?author=" + encodeURIComponent(new URLSearchParams(location.search).get("author") || ""), {credentials:"same-origin"}).then(response => {
-	if(!response.ok) response.text().then(text => { error(text); });
+	if(!response.ok) response.text().then(text => error(text));
 	else {
 		response.json().then(json => {
 			if(!json || !json.length)
@@ -104,6 +106,6 @@ if(location.pathname == "/" || location.pathname == "index.html") fetch("/capsul
 
 			for(var i = 0; i < json.length; i++)
 				renderCapsule($body, template, json[i]);
-		}).catch((e) => error("/capsules failed" + e));
+		}).catch(e => error("get /capsules failed: " + e));
 	}
 });
