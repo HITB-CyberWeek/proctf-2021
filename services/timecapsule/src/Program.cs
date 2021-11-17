91216b04f7fdd9c2677f70e4ea72e603ceee8738
Dmitriy Titarenko <mail.dscheg@gmail.com>
2021-11-17 14:22:30 +0500

    [timecapsule] service auto gen settings

--- services/timecapsule/src/Program.cs
+++ services/timecapsule/src/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Net;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 
@@ -12,16 +13,17 @@ namespace timecapsule
 		{
 			AppDomain.CurrentDomain.ProcessExit += (_, _) => DatabaseContext.Flush();
 
+			var settings = await Settings.ReadAsync(CancellationToken.None);
 			await new WebHostBuilder()
 				.UseKestrel(options =>
 				{
-					options.Listen(IPAddress.Any, 7007);
+					options.Listen(IPAddress.Any, settings.Port);
 					options.AddServerHeader = false;
 					options.Limits.KeepAliveTimeout = TimeSpan.FromSeconds(30.0);
-					options.Limits.MaxRequestBodySize = 2048L;
-					options.Limits.MaxRequestLineSize = 2048;
+					options.Limits.MaxRequestBodySize = settings.MaxRequestBodySize;
+					options.Limits.MaxRequestLineSize = settings.MaxRequestLineSize;
 					options.Limits.MaxRequestHeaderCount = 64;
-					options.Limits.MaxRequestHeadersTotalSize = 8192;
+					options.Limits.MaxRequestHeadersTotalSize = settings.MaxRequestHeadersTotalSize;
 					options.Limits.RequestHeadersTimeout = TimeSpan.FromSeconds(3.0);
 				}).UseContentRoot(Directory.GetCurrentDirectory()).UseStartup<Startup>().Build().RunAsync();
 		}
--- services/timecapsule/src/Settings.cs
+++ services/timecapsule/src/Settings.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
@@ -8,9 +9,28 @@ namespace timecapsule
 	public struct Settings
 	{
 		public int Port { get; set; }
+		public long MaxRequestBodySize { get; set; }
+		public int MaxRequestLineSize { get; set; }
+		public int MaxRequestHeadersTotalSize { get; set; }
 		public Guid Key { get; set; }
 
+		static Settings()
+		{
+			if(File.Exists(SettingsFilepath))
+				return;
+
+			Directory.CreateDirectory(Path.GetDirectoryName(SettingsFilepath));
+
+			using var stream = File.OpenWrite(SettingsFilepath);
+			JsonSerializer.SerializeAsync(stream, new Settings { Port = 7007, MaxRequestBodySize = 2048L, MaxRequestLineSize = 2048, MaxRequestHeadersTotalSize = 8192, Key = Guid.NewGuid() }, JsonSerializerOptions).GetAwaiter().GetResult();
+		}
+
+		public static Task<Settings> ReadAsync(CancellationToken token)
+			=> ReadAsync(new byte[4096], token);
 		public static async Task<Settings> ReadAsync(byte[] temp, CancellationToken token)
-			=> JsonSerializer.Deserialize<Settings>(new ReadOnlySpan<byte>(temp, 0, await Helper.ReadFileAsync("settings/timecapsule", temp, token)), new JsonSerializerOptions { ReadCommentHandling = JsonCommentHandling.Skip });
+			=> JsonSerializer.Deserialize<Settings>(new ReadOnlySpan<byte>(temp, 0, await Helper.ReadFileAsync(SettingsFilepath, temp, token)), JsonSerializerOptions);
+
+		private static readonly JsonSerializerOptions JsonSerializerOptions = new() { ReadCommentHandling = JsonCommentHandling.Skip, PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+		private const string SettingsFilepath = "settings/timecapsule";
 	}
 }
