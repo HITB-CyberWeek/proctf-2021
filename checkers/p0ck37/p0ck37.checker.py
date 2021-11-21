a18f34d845a094ed79e7ceefc71bf6ea2d5c81f8
Artem Zinenko <art@hackerdom.ru>
2021-11-21 10:24:38 +0500

    Configured production oauth in checker

--- checkers/p0ck37/p0ck37.checker.py
+++ checkers/p0ck37/p0ck37.checker.py
@@ -15,7 +15,7 @@ from checker_helper import *
 PORT = 3000
 TIMEOUT = 10
 OAUTH_TOKEN = 'd7ae69cd-7e91-44de-8729-12cded47b3f2'
-OAUTH_ENDPOINT = 'https://192.168.88.200:5000/'
+OAUTH_ENDPOINT = 'https://auth.ctf.hitb.org:5000/'
 FLAGS_API_KEY = '25807689-9ae1-4894-a6f8-940abd1c3a4a'
 FLAGS_ENDPOINT = 'http://192.168.88.200:8080/'
 
