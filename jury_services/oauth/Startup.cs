32e874eaa6ad52f6f96543067155848fac7cebd9
Artem Zinenko <art@hackerdom.ru>
2021-11-23 15:25:04 +0500

    Removed unused using in jury/oauth

--- jury_services/oauth/Startup.cs
+++ jury_services/oauth/Startup.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Security.Cryptography.X509Certificates;
-using System.Text;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
