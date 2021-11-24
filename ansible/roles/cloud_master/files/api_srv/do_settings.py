b553aa00d5669f28e8747f4d1c58657031ab83eb
Artem Zinenko <art@hackerdom.ru>
2021-11-24 10:26:51 +0500

    Changed c-2 to c4 for p0ck37

--- ansible/roles/cloud_master/files/api_srv/do_settings.py
+++ ansible/roles/cloud_master/files/api_srv/do_settings.py
@@ -19,7 +19,8 @@ CLOUDS = {
         "sizes": {
             "default": "c-2",
             "router": "s-2vcpu-4gb",
-            "empty": "s-8vcpu-16gb"
+            "empty": "s-8vcpu-16gb",
+            "p0ck37": "c-4"
         }
     },
     "bay": {
